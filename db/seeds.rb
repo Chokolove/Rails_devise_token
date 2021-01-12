003446262120139fc693a3693098e675ab3258a5
chokolove <carlosll2493@gmail.com>
2021-01-12 17:43:08 -0500

    Add some seed and foods views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do  
+  food = Food.new
+  food.name = Faker::Food.dish
+  food.price = rand(10..50)
+  food.save
+end
